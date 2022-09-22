3a59e5b786b8dd500ed1b97b71feee78ff2bfc5f
hamonanganardiansyah <hayymons123@gmail.com>
2022-09-22 20:43:05 +0700

    latihan ngoding

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya monang_ardiansyah
+ Saya tingga di daerah bekasi
+ olah raga yang saya sukai nge gym dan futsal
+ 
+ }
+}
