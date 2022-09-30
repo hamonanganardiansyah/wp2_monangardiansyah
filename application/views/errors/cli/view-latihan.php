1a43929b21bc99aa8ade907ceafde8c27ec5fdc1
hamonanganardiansyah <hayymons123@gmail.com>
2022-09-30 16:06:51 +0700

    latihan_view

---
+++ application/views/errors/cli/view-latihan.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>latihan 1</title>
+</head>
+
+<body>
+    halo kawan.. yuk kita belajar web programming..!!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan 
+ $nilai1 . " + " .$nilai2 . " = " . $hasil; ?>
+ 
+</body>
+
+</html>
