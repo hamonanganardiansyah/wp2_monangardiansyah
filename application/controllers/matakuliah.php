a85e4574aca8f8eeb1e0c7e1704a4d4a9769f9b9
hamonanganardiansyah <hayymons123@gmail.com>
2022-10-14 16:24:37 +0700

    membuat model

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -7,11 +7,25 @@ class Matakuliah extends CI_Controller
  }
  public function cetak()
  {
- $data = [
- 'kode' => $this->input->post('kode'),
- 'nama' => $this->input->post('nama'),
- 'sks' => $this->input->post('sks')
- ];
- $this->load->view('view-data-matakuliah', $data);
-}
+    $this->form_validation->set_rules('kode,' 'kode mata kuliah'required|min_length[3]',[
+        'required' => 'kode matakuliah harus diisi',
+        'min_length' => 'kode terlalu pendek'
+    
+    $this->form_validation->set_rules('nama', 'nama matakuliah 
+    'required|min_length[3]', [
+                 'required' => 'nama matakuliah harus diisi
+                 min_length' => 'nama terlalu pendek
+    ]);
 
+    if ($this->form_validation->run() != true) {
+        $thid->load->view('view-form-matakuliah');
+    } else {
+        $data = [
+            'kode' => $this->input->post('kode'),
+            'nama' => $this->input->post('nama'),
+            'sks' => $this->input->post('sks')
+        ];
+        $this->load->view('-data-matakuliah', $data);
+    }
+ }
+}
